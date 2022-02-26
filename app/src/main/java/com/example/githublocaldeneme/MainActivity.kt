d8a322b2b1d3eef58d852d7167c4930d9aed800f
Hatice Sarp <hatice.sarp@vlmedia.com.tr>
2022-02-26 15:16:17 +0300

    fix

---
+++ app/src/main/java/com/example/githublocaldeneme/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.githublocaldeneme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("github","inital log")
+
+
+        Log.d("github","inital log")
+
+
+        Log.d("hatice","")
+        Log.d("hatice","")
+
+    }
+}
